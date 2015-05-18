ae2e042c64d0abb90e7c7774c9cec3d34e207066
patitonav <pato.lalala@gmail.com>
2015-05-18 14:48:02 -0300

    Main.py

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
 #   Main.py
+
+# Creates a matrix with the map according to the 
+# file loaded
+def loadMap(file):
+    return map
+
+def main():
+    print "Hello World"
+    map = loadMap(file)
+    
+    
+if __name__ == '__main__':
+    main()
