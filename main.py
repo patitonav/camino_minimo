a99697c5b6e58f52ab444a9c01f35bdf476aa973
Fran <pancho87@gmail.com>
2015-06-09 01:11:32 -0300

    Refactor + fix del laberinto + encontrar punto inicial + actualizar posición de la solución + imprimir solución final.

--- main.py
+++ main.py
@@ -1,100 +1,128 @@
-#   Main.py
-import numpy #sudo apt-get install python-numpy
-
+# This Python file uses the following encoding: utf-8
+import numpy  # sudo apt-get install python-numpy
+import random
 
 # Matrix
-#-------
+# -------
 # 9: Wall
 # 8: Starting Point
 # 7: Finish Point
-# (1-6): Diferent obstacles
+# (1-6): Different obstacles
 # 0: Walkable
-class solution:
-    def __init__(self, matrix):
-        self.chromozones = []
+FINISH_POINT = 7
+STARTING_POINT = 8
+
+
+class Solution:
+    def __init__(self, maze):
+        self.chromosomes = []
         self.fitness = 99999
-        self.position_x
-        self.position_y
-        self.run(matrix)
-    
-    # Make a solution for the maze and get fitness
-    # and sets the fitness
-    def run(self, matrix):
-        self.set_statingPoint()
-        position =  0
+        self.position_x = maze.position_x
+        self.position_y = maze.position_y
+        self.matrix = maze.matrix
+
+    # Make a solution for the maze and get fitness and sets the fitness
+    def run(self):
+        matrix = self.matrix
+        position = 0
         self.fitness = 0
         last_step = 4
-        while (position != 7):
+        while position != FINISH_POINT:
             step = random.randint(0, 3)
-            #Goes Right
-            if (step == 0) and(matrix[self.position_x+1][self.position_y] != 9) and (last_step != 2):
-                position = matrix[self.position_x+1][self.position_y]
-                self.fitness = self.fitness + 1 + position
-                self.chromozones.append(step)
-                last_step = step
-            #Goes Up
-            if (step == 1) and(matrix[self.position_x][self.position_y+1] != 9) and (last_step != 3):
-                position = matrix[self.position_x][self.position_y+1]
-                self.fitness = self.fitness + 1 + position
-                self.chromozones.append(step)
-                last_step = step
-            #Goes Left
-            if (step == 2) and(matrix[self.position_x-1][self.position_y] != 9) and (last_step != 0):
-                position = matrix[self.position_x-1][self.position_y]
-                self.fitness = self.fitness + 1 + position
-                self.chromozones.append(step)   
-                last_step = step
-            #Goes Down
-            if (step == 3) and (matrix[self.position_x][self.position_y-1] != 9) and (last_step != 1):
-                position = matrix[self.position_x][self.position_y-1]
-                self.fitness = self.fitness + 1 + position
-                self.chromozones.append(step)
-                last_step = step
-            
-        return
-    
-    # Finds and set the stating position    
-    def set_statingPoint(self):
-        return
+            try:
+                # Goes DOWN
+                if (step == 0) and (matrix[self.position_x + 1][self.position_y] != 9) and (last_step != 2):
+                    position = matrix[self.position_x + 1][self.position_y]
+                    self.fitness = self.fitness + 1 + position
+                    self.chromosomes.append(step)
+                    last_step = step
+                    self.position_x += 1
+                # Goes RIGHT
+                elif (step == 1) and (matrix[self.position_x][self.position_y + 1] != 9) and (last_step != 3):
+                    position = matrix[self.position_x][self.position_y + 1]
+                    self.fitness = self.fitness + 1 + position
+                    self.chromosomes.append(step)
+                    last_step = step
+                    self.position_y += 1
+                # Goes UP
+                elif (step == 2) and (matrix[self.position_x - 1][self.position_y] != 9) and (last_step != 0):
+                    position = matrix[self.position_x - 1][self.position_y]
+                    self.fitness = self.fitness + 1 + position
+                    self.chromosomes.append(step)
+                    last_step = step
+                    self.position_x -= 1
+                # Goes LEFT
+                elif (step == 3) and (matrix[self.position_x][self.position_y - 1] != 9) and (last_step != 1):
+                    position = matrix[self.position_x][self.position_y - 1]
+                    self.fitness = self.fitness + 1 + position
+                    self.chromosomes.append(step)
+                    last_step = step
+                    self.position_y -= 1
+                # No lo borren sirve para encontrar puntos sin salida en laberintos
+                # print "%d,%d" % (self.position_x, self.position_y)
+            except IndexError as error:
+                print error
+
     # Get fitness for the solution    
     def get_fitness(self):
         return self.fitness
 
     # Print the solution in screen    
     def print_solution(self):
-        return
+        print self.matrix
+        # TODO: imprimir matriz resuelta
+        print "Camino mínimo: " + ', '.join([str(chrom) for chrom in self.chromosomes])
 
-# Contains all the information about the Maze 
-# and how to solve it
-class maze:
+
+# Contains all the information about the Maze  and how to solve it
+class Maze():
+
     # Define initials parameters
     def __init__(self, population, iterations):
-        self.finish =  false
-        self.matrix = numpy.zeros(shape=(8,8))
+        self.finish = False
+        self.matrix = numpy.zeros(shape=(8, 8))
         self.population = []
         self.population_number = population
         self.iteration = 0
         self.max_iteration = iterations
+        self.position_x = None
+        self.position_y = None
+
+    def set_stating_point(self):
+
+        x = 0
+        for row in self.matrix:
+            y = 0
+            for col in row:
+                if col == STARTING_POINT:
+                    self.position_x = x
+                    self.position_y = y
+                    break
+                y += 1
+            if self.position_x and self.position_y:
+                break
+            x += 1
+
     # Load the map with the access and exit point
-    def loadMap(self):
+    def load_map(self):
         # Hardcodear matris al principio, despues vemos si hacemos otra cosa
-
         for i in range(0, 8):
-            self.matrix[i][0]=9
+            self.matrix[i][0] = 9
         for j in range(0, 8):
-            self.matrix[7][j]=9
+            self.matrix[7][j] = 9
         for j in range(0, 8):
-            self.matrix[0][j]=9
+            self.matrix[0][j] = 9
         for i in range(0, 8):
-            self.matrix[i][7]=9
-
+            self.matrix[i][7] = 9
         self.matrix[6][1] = 9
         self.matrix[6][2] = 9
         self.matrix[6][3] = 9
         self.matrix[3][6] = 7
         self.matrix[1][7] = 8
         self.matrix[2][2] = 9
+        self.matrix[2][3] = 9
         self.matrix[3][2] = 9
+        self.matrix[3][3] = 9
         self.matrix[4][2] = 9
         self.matrix[4][3] = 9
         self.matrix[2][4] = 9
@@ -105,48 +133,59 @@ class maze:
         self.matrix[4][5] = 9
         self.matrix[5][5] = 9
         self.matrix[2][6] = 9
+        # print self.matrix
+        self.set_stating_point()
 
-        print matrix
-
-        return
     # Creates the initial population
     def init_population(self):
         for i in range(self.population_number):
-            self.population.append(solution(self.matrix))
-        return
+            a_solution = Solution(self)
+            a_solution.run()
+            self.population.append(a_solution)
+
     # Evaluates if we arrived to a good solution
-    def finish(self):
-        if self.iteration == self.max_iteration:
-            self.finish = true
-        if self.population[0].get_fitness <= self.best_posible:
-            self.finish = true
+    def has_finished(self):
+        if self.iteration >= self.max_iteration:
+            self.finish = True
+        # TODO: Dar un valor a self.best_posible
+        # elif self.population[0].get_fitness() <= self.best_posible:
+        #     self.finish = True
         return self.finish
+
     # Order the list by fitness
     def calc_fitness(self):
-        self.population.sort(key = get_fitness())
-        return
-    # Mutates the solutions according to the 
+        self.population.sort(key=lambda solution: solution.get_fitness())
+        pass
+
+    # Mutates the solutions according to the
     # parameters passed on the init
     def mutate(self):
-        return 
-    # Mates the solutions according to the 
+        pass
+        # Mates the solutions according to the
+
     # parameters passed on the initialization
     def mate(self):
-        return 
-    # Return the solution with best fitness score
-    def getWinner(self):
+        pass
+        # Return the solution with best fitness score
+
+    def get_winner(self):
         return self.population[0]
-    
+
+
 def main():
-    maze = maze()
-    maze.loadMap()
+    # TODO: pasar parámetros al constructor
+    maze = Maze(3, 10)
+    maze.load_map()
     maze.init_population()
     maze.calc_fitness()
-    while (maze.finish() == false):
+    while not maze.has_finished():
         maze.mutate()
         maze.mate()
         maze.calc_fitness()
-    winner = maze.getWinner()
+        maze.iteration += 1
+    winner = maze.get_winner()
     winner.print_solution()
+
+
 if __name__ == '__main__':
     main()
